54ef868f01bc1211270e77ec3eacb4051f4676d3
Joanna Cieszyńska <joannacieszynska33@gmail.com>
2020-05-13 16:48:29 +0200

    test scenarios

---
+++ Projekt Podstawy Programowania/projekt/projekt.UnitTests/Form1Tests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projekt.UnitTests
+{
+    class Form1Tests
+    {
+        [Test]
+        public void PoziomTick_WhenCalled_MoveTheBallHorizontallyRight()
+        {
+
+        }
+        [Test]
+        public void PoziomTick_WhenCalled_MoveTheBallHorizontallyLeft()
+        {
+
+        }
+        [Test]
+        public void Form1KeyDown_WhenLeftKeyPressed_MoveTheSliderLeft()
+        {
+
+        }
+        [Test]
+        public void Form1KeyDown_WhenRightKeyPressed_MoveTheSliderRight()
+        {
+
+        }
+        [Test]
+        public void Form1KeyUp_WhenTheKeyNotPressed_StopTheSlider()
+        {
+
+        }
+        [Test]
+        public void PionTick_WhenTheBallHitsTheGround_EndTheGame()
+        {
+
+        }
+        [Test]
+        public void PionTick_WhenCalled_MoveTheBallVerticallyUp()
+        {
+
+        }
+        [Test]
+        public void PionTick_WhenCalled_MoveTheBallVerticallyDown()
+        {
+
+        }
+        [Test]
+        public void PionTick_WhenTheBallHitsTheObstacle_RemoveTheObstacleAndChangeTheDirection()
+        {
+
+        }
+        [Test]
+        public void PionTick_WhenTheObstacleIsRemoved_AddAPointToTheScore()
+        {
+
+        }
+    }
+}
